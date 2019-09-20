f9afff2e7b47010daf7748fb233227c7d0087b3b
John Christopher <john.christopher@c2fo.com>
2019-09-20 14:23:25 -0500

    adding tests

---
+++ test/rumbl/accounts_test.exs
@@ -0,0 +1,65 @@
+defmodule Rumbl.AccountsTest do
+  use Rumbl.DataCase, async: true
+
+  alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
+
+  describe "register_user/1" do
+    @valid_attrs %{
+      name: "User",
+      username: "eva",
+      password: "secret"
+    }
+    @invalid_attrs %{}
+
+    test "with valid data inserts user" do
+      assert {:ok, %User{id: id}=user } = Accounts.register_user(@valid_attrs)
+      assert user.name == "User"
+      assert user.username == "eva"
+      assert [%User{id: ^id}] = Accounts.list_users()
+    end
+
+    test "with invalid data doesn't insert user" do
+      assert{:error,  _changeset} = Accounts.register_user(@invalid_attrs)
+      assert Accounts.list_users() == []
+    end
+
+    test "enforces unique usernames" do
+      assert {:ok, %User{id: id}=user } = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset} = Accounts.register_user(@valid_attrs)
+      assert %{username: ["has already been taken"]} = errors_on(changeset)
+    end
+
+    test "Registration does not accept long username" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+      assert {:error, changeset} = Accounts.register_user(attrs)
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+
+    test "Registration requires password to be at least 6 characters long" do
+      attrs = Map.put(@valid_attrs, :password, "12345")
+      assert {:error, changeset} = Accounts.register_user(attrs)
+      assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+
+  end
+
+  describe "authenticate_by_username_and_pass/2" do
+
+    @pass "123456"
+
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      IO.puts user.id
+      IO.puts @pass
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_pass(user.username, @pass)
+      assert auth_user.id == user.id
+    end
+  end
+
+end
